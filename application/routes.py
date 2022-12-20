ac027783f0ebbe9be05eb4745bf94539edeebf98
Juan Simonetti <sjuanandres0@gmail.com>
2022-12-20 07:42:45 +0100

    URL <>

--- application/routes.py
+++ application/routes.py
@@ -11,12 +11,13 @@ def index():
 def login():
     return render_template("login.html", login=True)
 
-@app.route("/courses")
-def courses():
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term="Spring 2019"):
     courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
     print(courseData)
     print(courseData[0]["title"])
-    return render_template("courses.html", courseData=courseData, courses=True)
+    return render_template("courses.html", courseData=courseData, courses=True, term=term)
 
 @app.route("/register")
 def register():
