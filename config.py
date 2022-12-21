0f83c1afd01680ff32f4d6258b17157e716a8b6b
Juan Simonetti <sjuanandres0@gmail.com>
2022-12-21 19:43:56 +0100

    mongo init

--- config.py
+++ config.py
@@ -2,5 +2,10 @@ import os
 
 class Config(object):
     #special key that is used to protect the data transfer. Use that key to check against the cookie file.
-    SECRET_KY = os.environ.get('SECRET_KEY') or "secret_string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
     
+    MONGODB_SETTINGS = { 
+        'db' : 'UTA_Enrollment'
+        # ,'host' : 'mongodb://localhost:27017/UTA_Enrollment'
+    }
+
